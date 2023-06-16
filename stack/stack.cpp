cb5a1ec56ef4d3770430ebd77ffcf216397915a4
mddhafa <mdmuhammaddhafa1234@gmail.com>
2023-06-16 15:00:26 +0700

    Add project files.

---
+++ stack/stack.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include<string>
+
+using namespace std;
+
+class StackArray {
+private:
+	int stack_array[5];
+	int top;
+public:
+	//constructor
+	StackArray() {
+		top = -1;
+	}
+
+	int push
+};
